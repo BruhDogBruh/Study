a5f83789786f1050f26ee67873f95058568c8934
Abdulrahman <a14azrak@gmail.com>
2024-02-27 09:14:04 +0100

    Initial Commit

---
+++ receptAPI/receptAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  receptAPIApp.swift
+//  receptAPI
+//
+//  Created by Abdulrahman.Alazrak on 2024-02-27.
+//
+
+import SwiftUI
+
+@main
+struct receptAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
